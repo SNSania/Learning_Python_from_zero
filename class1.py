8519f5a131eb8ce76fb9e0ad796be67f1765c021
SNSania <150348542+SNSania@users.noreply.github.com>
2024-02-24 19:07:52 +0500

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,9 @@
+print("Hello World this is Pakistan and I am Sania Nawaz from Pakistan")
+print("Pakistan Zindabad")
+name = "Sania Nawaz"
+#name = 700
+print(name)
+print(type(name))
+print(id(name))
+#print(dir(name))
+print([i for i in dir(name) if "__" not in i])
---
+++ class2.py
@@ -0,0 +1,4 @@
+#if we write "print(name)" it will genrate error becouse the name is not defind before to remove error we need to put "name" so it consider it as string
+print("name") #correct formate
+name: str = "Sania Nawaz" # or declaring before the type 
+print(name) #correct now because name is already defined 
